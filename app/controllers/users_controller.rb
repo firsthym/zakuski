2b0234f44bf6ca069be049878dcbb41f109f5995
Ming <firsthym@gmail.com>
2012-09-17 22:39:11 +0800

    edit for user WIP

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,6 +61,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.update_attributes(params[:user])
+        sign_in @user
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { head :no_content }
       else
