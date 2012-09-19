ea43113ed4a84e643e96f00f7fce0be7999359eb
Ming <firsthym@gmail.com>
2012-09-19 22:46:44 +0800

    add db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create(title: '新闻')
+Category.create(title: '体育')
+Category.create(title: '娱乐')
+Category.create(title: '视频')
+Category.create(title: '财经')
+Category.create(title: '女人')
+Category.create(title: '科技')
+Category.create(title: '数码')
+Category.create(title: '手机')
+Category.create(title: '汽车')
+Category.create(title: '旅游')
+Category.create(title: '房产')
+Category.create(title: '游戏')
+Category.create(title: '生活')
