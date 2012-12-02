ffa46bd458808e85c16854c429761f5958951068
Ming <firsthym@gmail.com>
2012-12-02 19:19:01 +0800

    check if annotations.present?

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,6 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Node.delete_all
-Node.create(title: '兴趣', description: '游戏 电影 音乐 汽车 创意 艺术 旅行 文化')
-Node.create(title: '职业', description: '科技 金融 教育 媒体 服务 建筑 人力')
 Node.create(title: '信息', description: '视频 购物 好玩 分享 城市 交易 工作 问答')
+Node.create(title: '职业', description: '科技 金融 教育 媒体 服务 建筑 人力')
+Node.create(title: '兴趣', description: '游戏 电影 音乐 汽车 创意 艺术 旅行 文化')
