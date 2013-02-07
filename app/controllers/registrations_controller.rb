5bbcc9afb960a1a27a942cc8c99ae355346afc27
firsthym <firsthym@gmail.com>
2013-02-07 06:41:28 +0000

    hot fix: crash if error happens on registering

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
-	def new
-	  @hash_tag = Hash.new
-      @hash_node = Hash.new
-      Tag.all.group_by { |t| t.name }.collect { |k,v| @hash_tag[k]= v[0]}
-      Node.all.group_by { |n| n.title }.collect { |k,v| @hash_node[k]= v[0]}
-	  super
-	end
+	before_filter :init_link, only: [:new, :create]
+	private
+		def init_link
+			@hash_tag = Hash.new
+      			@hash_node = Hash.new
+	      		Tag.all.group_by { |t| t.name }.collect { |k,v| @hash_tag[k]= v[0]}
+      			Node.all.group_by { |n| n.title }.collect { |k,v| @hash_node[k]= v[0]}	
+		end
 end
