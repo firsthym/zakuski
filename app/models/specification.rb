c9791fe87ddc1d3b84c52f3ed6840c3461877954
firsthym <firsthym@gmail.com>
2013-02-16 07:49:04 +0000

    fix bug for single quot

--- app/models/specification.rb
+++ app/models/specification.rb
@@ -3,11 +3,11 @@ class Specification
 
 	field :title, type: String
 	field :description, type: String
-    field :search_mode, type: String, default: 'filter'
-    field :mode_weight, type: Integer, default: 10
-    field :search_image, type: Boolean, default: false
-    field :search_thumbnail, type: Boolean, default: true 
-    field :sort_by, type: String, default: 'relevance'
+    	field :search_mode, type: String, default: 'boost'
+    	field :mode_weight, type: Integer, default: 1
+    	field :search_image, type: Boolean, default: false
+    	field :search_thumbnail, type: Boolean, default: true 
+    	field :sort_by, type: String, default: 'relevance'
 
 	attr_accessible :title, :description, :search_mode,
                      :search_image, :sort_by, :search_thumbnail
