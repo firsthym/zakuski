ce50bcabc5457d9a78d3d4a16930a49a502c2cfa
firsthym <firsthym@gmail.com>
2013-04-10 18:24:59 +0800

    enabel mini_magick

--- app/uploaders/thumbnail_uploader.rb
+++ app/uploaders/thumbnail_uploader.rb
@@ -4,7 +4,7 @@ class ThumbnailUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
   # include Sprockets::Helpers::RailsHelper
@@ -29,7 +29,7 @@ class ThumbnailUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :scale => [180, 180]
+  process :resize_to_fill => [180, 180]
   #
   # def scale(width, height)
   #   # do something
